396d35f33968016a9fa482c449d2fa2b814daacb
alessandroscaudo <75387875+alessandroscaudo@users.noreply.github.com>
2023-10-26 16:00:07 +0000

    modified:   components/Header.js 	modified:   package-lock.json 	modified:   package.json 	modified:   styles/globals.css

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Header({ title }) {
+function Header(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-  <nav className="navbar navbar-expand-lg navbar-light ">
-    <a className="navbar-brand" href="http://alessandroscaudo.com">
-      <h1>{title}</h1>
-    </a>
-    <button
-    className="navbar-toggler"
-    type="button"
-    data-toggle="collapse"
-    data-target="#navbarNav"
-    aria-controls="navbarNav"
-    aria-expanded="false"
-    aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>  
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <a className="navbar-brand" href="http://alessandroscaudo.com">
+        <h1>{props.title}</h1>
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded={isOpen ? 'true' : 'false'}
+        onClick={toggleMenu}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
+        <ul className="navbar-nav ml-auto ">
           <li className="nav-item">
             <a className="nav-link" href="#">
               Home
@@ -37,6 +43,8 @@ export default function Header({ title }) {
           </li>
         </ul>
       </div>
-  </nav>
-  )
+    </nav>
+  );
 }
+
+export default Header;
