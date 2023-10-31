65199074f9a9abcce0c3113db6d7bb0510c4f9c1
alessandroscaudo <75387875+alessandroscaudo@users.noreply.github.com>
2023-10-31 10:19:48 +0000

    modified:   components/DownloadCV.js

--- components/DownloadCV.js
+++ components/DownloadCV.js
@@ -1,69 +1,60 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
-
-// Inizializza Firebase con la tua configurazione
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-  };
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-function DownloadCV() {
-    const [email, setEmail] = useState('')
+const EmailForm = () => {
+    
+    const [formData, setFormData] = useState({
+    email: ''
+    });
 
-    const emailChange = (e) => {
-        const inputValue = e.target.value;
-        setEmail(inputValue);
-        // Verifica la validità dell'indirizzo email
-        //const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-        //setIsEmailValid(regex.test(inputValue));
-      };
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+  };
 
-    const scaricaCV = async() => {
-        const emailData = {
-            email: email, 
-            timestamp: new Date(), 
-        };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
         try {
-            await addDoc(collection(db, 'emailCV'), emailData)
+            const docRef = await addDoc(collection(db, 'emailCV'), formData);
+            console.log('Document written with ID: ', docRef.id);
+            // Esegui azioni aggiuntive qui se necessario
         } catch (error) {
-            console.error('Errore durante il salvataggio su Firebase: ', error);
+            console.error('Error adding document: ', error);
         }
     };
 
     return (
         <div className="containerHigh">
-            <h2>Scarica il mio Curriculum Vitae</h2>
-            <p>Inserisci il tuo indirizzo email per scaricare il CV.</p>
-            <div className="input-group mb-3">
-                <input
-                id="email"
-                type="email"
-                placeholder="Indirizzo email..."
-                className=''
-                value={email}
-                onChange={emailChange}
-                />
-                <div className="input-group-append mx-3">
-                    <button 
-                    className="btn btn-flat"
-                    onClick={scaricaCV}>
-                        Scarica il CV
-                    </button>
-                </div>
-            </div>
-            <div>
-                <p style={{fontWeight:"lighter"}}>
-                    Premendo "Scarica il CV", accetti la nostra <a style={{fontWeight:'bold'}} href="/privacyPolicy">Informativa sulla privacy</a>.
-                </p>
-            </div>
+        <h2>Scarica il mio Curriculum Vitae</h2>
+        <p>Inserisci il tuo indirizzo email per scaricare il CV.</p>
+        <div className="input-group mb-3">
+            <form onSubmit={handleSubmit}>
+            <input type="email" name="email" placeholder="Email" onChange={handleChange} />
+            <button type="submit">Invia</button>
+            </form>
         </div>
-    )
-}
+        <div>
+            <p style={{ fontWeight: 'lighter' }}>
+            Premendo "Scarica il CV", accetti la nostra{' '}
+            <a style={{ fontWeight: 'bold' }} href="/privacyPolicy">
+                Informativa sulla privacy
+            </a>
+            .
+            </p>
+        </div>
+        </div>
+    );
+};
 
-export default DownloadCV;
+export default EmailForm;
