3ec51e305f7d08fae0b3671032f050780409822b
alessandroscaudo <75387875+alessandroscaudo@users.noreply.github.com>
2023-10-31 10:32:55 +0000

    modified:   components/DownloadCV.js

--- components/DownloadCV.js
+++ components/DownloadCV.js
@@ -1,60 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-};
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+  };
+  
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
-const EmailForm = () => {
-    
-    const [formData, setFormData] = useState({
-    email: ''
-    });
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-  };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
+ 
+const DownloadCV = () => {
+    const [todo, setTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+ 
+    const addTodo = async (e) => {
+        e.preventDefault();  
+       
         try {
-            const docRef = await addDoc(collection(db, 'emailCV'), formData);
-            console.log('Document written with ID: ', docRef.id);
-            // Esegui azioni aggiuntive qui se necessario
-        } catch (error) {
-            console.error('Error adding document: ', error);
-        }
-    };
-
+            const docRef = await addDoc(collection(db, "emailCV"), {
+              todo: todo,    
+            });
+            console.log("Document written with ID: ", docRef.id);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
+    }
+ 
+    const fetchPost = async () => {
+       
+        await getDocs(collection(db, "emailCV"))
+            .then((querySnapshot)=>{              
+                const newData = querySnapshot.docs
+                    .map((doc) => ({...doc.data(), id:doc.id }));
+                setTodos(newData);                
+                console.log(todos, newData);
+            })
+       
+    }
+   
+    useEffect(()=>{
+        fetchPost();
+    }, [])
+ 
+ 
     return (
-        <div className="containerHigh">
-        <h2>Scarica il mio Curriculum Vitae</h2>
-        <p>Inserisci il tuo indirizzo email per scaricare il CV.</p>
-        <div className="input-group mb-3">
-            <form onSubmit={handleSubmit}>
-            <input type="email" name="email" placeholder="Email" onChange={handleChange} />
-            <button type="submit">Invia</button>
-            </form>
-        </div>
-        <div>
-            <p style={{ fontWeight: 'lighter' }}>
-            Premendo "Scarica il CV", accetti la nostra{' '}
-            <a style={{ fontWeight: 'bold' }} href="/privacyPolicy">
-                Informativa sulla privacy
-            </a>
-            .
-            </p>
-        </div>
-        </div>
-    );
-};
-
-export default EmailForm;
+        <section className="todo-container">
+            <div className="todo">
+                <h1 className="header">
+                    Todo-App
+                </h1>
+   
+                <div>
+   
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="What do you have to do today?"
+                            onChange={(e)=>setTodo(e.target.value)}
+                        />
+                    </div>
+   
+                    <div className="btn-container">
+                        <button
+                            type="submit"
+                            className="btn"
+                            onClick={addTodo}
+                        >
+                            Submit
+                        </button>
+                    </div>
+   
+                </div>
+   
+                <div className="todo-content">
+                    {
+                        todos?.map((todo,i)=>(
+                            <p key={i}>
+                                {todo.todo}
+                            </p>
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
+    )
+}
+ 
+export default DownloadCV
