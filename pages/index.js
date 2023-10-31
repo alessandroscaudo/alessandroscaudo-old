f4a66d0860da83ac2ba7622fd7cb2edc1829e81f
alessandroscaudo <75387875+alessandroscaudo@users.noreply.github.com>
2023-10-31 15:21:28 +0000

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <html lang="it" />
+        
         <title>Alessandro Scaudo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
