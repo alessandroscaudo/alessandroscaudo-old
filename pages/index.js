cf414bb8439a81a6a105bc62b7d9dd199d35904e
alessandroscaudo <75387875+alessandroscaudo@users.noreply.github.com>
2023-10-31 15:14:23 +0000

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import AppShare from '@components/AppShare'
-import DownloadCV from '@components/DownloadCV'
+//import DownloadCV from '@components/DownloadCV'
 import Footer from '@components/Footer'
 
 export default function Home() {
